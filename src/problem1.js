325f8684f66eb71d52ff019921bb3e446cd7cbd0
heonq <gkagjsrb@naver.com>
2023-07-18 22:47:23 +0900

    feat(PROBLEM1): 전달받은 페이지의 왼쪽 페이지가 1 이하이거나 오른쪽 페이지가 400 이상인지 확인하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -5,6 +5,7 @@ function problem1(pobi, crong) {
 
 const validatePages = (pages) => {
   if (pages[1] - pages[0] !== 1) return false;
+  if (pages[0] <= 1 || pages[1] >= 400) return false;
 };
 
 module.exports = problem1;
