764c34e294490a7f8bac5f758d3b292f3a0eb8eb
heonq <gkagjsrb@naver.com>
2023-07-19 14:13:23 +0900

    feat(PROBLEM4): charCode 값을 저장하는 객체 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+const charCodeValue = {
+  A: 65,
+  Z: 90,
+  a: 97,
+  z: 122,
+  lowercase: 219,
+  uppercase: 155,
+};
+
+function problem4(word) {}
 
 module.exports = problem4;
