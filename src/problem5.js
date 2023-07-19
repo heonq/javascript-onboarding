5dd99caa08717a9581102004589b9b13a8c8ac8a
heonq <gkagjsrb@naver.com>
2023-07-19 17:48:52 +0900

    feat(PROBLEM5): 주어진 돈을 화폐 단위로 나눈 값을 배열에 저장하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,10 @@
+const unitArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const amountArray = [];
+  for (let i = 0; i < unitArray.length; i++) {
+    amountArray.push(Math.floor(money / unitArray[i]));
+  }
 }
 
 module.exports = problem5;
