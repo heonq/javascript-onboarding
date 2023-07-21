42bc3d291713bc78a603a6729164e3efaa474b5e
heonq <gkagjsrb@naver.com>
2023-07-21 11:17:34 +0900

    fix(PROBLEM7): friends배열에 대한 map 메서드를 forEach로 변경

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,7 @@ function problem7(user, friends, visitors) {}
 
 const getFriendOfUser = (user, friends) => {
   let friendOfUser = [];
-  friends.map((relationship) => {
+  friends.forEach((relationship) => {
     if (relationship.includes(user)) {
       mutualFriend.push(relationship[1 - relationship.indexOf(user)]);
     }
