e5c95cf99b01d2811d3c334464f859799ec946a3
heonq <gkagjsrb@naver.com>
2023-07-21 10:38:42 +0900

    feat(PROBLEM6): 중복된 닉네임을 맵에 추가하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
-function problem6(forms) {
-  var answer;
-  return answer;
-}
+function problem6(forms) {}
+
+const getDuplicatedEmail = (forms) => {
+  const twoLetterAndEmail = new Map();
+  const duplicatedUserNickName = new Set();
+
+  forms.forEach(([email, nickName]) => {
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const twoLetter = nickName.substr(i, i + 2);
+      if (twoLetterAndEmail.has(twoLetter)) {
+        duplicatedUserNickName.add(email);
+        duplicatedUserNickName.add(twoLetterAndEmail.get(twoLetter));
+      }
+    }
+  });
+};
 
 module.exports = problem6;
